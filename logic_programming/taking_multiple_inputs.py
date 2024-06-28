1192601df564fe332c45e4dbe2f1292377964aba
Namir50 <namirsayyed50@gmail.com>
2024-06-28 14:41:56 +0530

    New changes

--- logic_programming/taking_multiple_inputs.py
+++ logic_programming/taking_multiple_inputs.py
@@ -3,3 +3,9 @@ a, b, c = (input("Enter three nummbers: ")).split()
 print("The value of a is", a)
 print("The value of b is", b)
 print("The value of c is", c)
+
+
+x , y, z = [int(x) for x in input("Enter three numbers: ").split()]
+print("The value of x is ",x)
+print("The value of y is ",y)
+print("The value of z is ",z)
